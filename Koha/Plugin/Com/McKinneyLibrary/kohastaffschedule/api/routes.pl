5b39d7894546e9d49e9720e5ae2dafb4963f2ce0
mckinneylibrary <56939901+mckinneylibrary@users.noreply.github.com>
2026-05-20 07:51:25 -0500

    Update get_schedule to filter by shift_date
    Modified get_schedule to accept date range parameters.

--- Koha/Plugin/Com/McKinneyLibrary/kohastaffschedule/api/routes.pl
+++ Koha/Plugin/Com/McKinneyLibrary/kohastaffschedule/api/routes.pl
@@ -1,6 +1,12 @@
 # Inside routes.pl - using Perl DBI
 sub get_schedule {
-    my $dbh = C4::Context->dbh;
-    my $data = $dbh->selectall_arrayref("SELECT * FROM plugin_ks_assignments", {Slice=>{}});
+    my ($self, $c) = @_;
+    my $dbh  = C4::Context->dbh;
+    my $from = $c->param('from');
+    my $to   = $c->param('to');
+    my $data = $dbh->selectall_arrayref(
+        "SELECT * FROM plugin_ks_assignments WHERE shift_date BETWEEN ? AND ?",
+        {Slice=>{}}, $from, $to
+    );
     return $c->render(json => $data);
 }
